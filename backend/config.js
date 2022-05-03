6a4c85d938621e8ac4caad0475d6e52a29a917f5
surawit-srw <62070210@it.kmitl.ac.th>
2022-05-03 10:41:26 +0700

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: "database-1.cp3wri63znyw.us-east-1.rds.amazonaws.com",
-  user: "admin",
-  password: "12345678",
-  database: "database-1",
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "project",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
